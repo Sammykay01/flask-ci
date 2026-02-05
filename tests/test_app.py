af3f6b5d68feb78449e15b5f2be68e25cdf80690
Samuel Udeh <udehsamuel69@email.com>
2026-02-05 03:35:19 +0000

    Fix Flask package structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,9 @@
-from app import app
+from app import create_app
 
 def test_home_returns_200():
+    app = create_app()
     client = app.test_client()
     res = client.get("/")
     assert res.status_code == 200
     assert res.get_json()["message"] == "Hello from Flask CI!"
+
